f0f8261c68f33d877464dc61db20baaac3287ace
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-03-14 18:07:56 +0100

    Removed gradient

---
+++ Assets/Fueler/Scripts/Runtime/Content/Meta/Ui/MainMenu/Widgets/SocialMediaButtonWidget.cs
@@ -0,0 +1,29 @@
+﻿using Juce.CoreUnity.Ui.Others;
+using UnityEngine;
+
+namespace Fueler.Content.Meta.Ui.MainMenu.Widgets
+{
+    public class SocialMediaButtonWidget : MonoBehaviour
+    {
+        [Header("Input")]
+        [SerializeField] private PointerAndSelectableSubmitCallbacks pointerAndSelectableSubmitCallbacks = default;
+
+        [Header("Url")]
+        [SerializeField] private string url = default;
+
+        private void Awake()
+        {
+            pointerAndSelectableSubmitCallbacks.OnSubmit += OnSubmit;
+        }
+
+        private void OnDestroy()
+        {
+            pointerAndSelectableSubmitCallbacks.OnSubmit -= OnSubmit;
+        }
+
+        private void OnSubmit()
+        {
+            Application.OpenURL(url);
+        }
+    }
+}
